1d9b19726bd485526d2ac7a139128ddfb3d44856
Cesar Grossl <cgrossl00@gmail.com>
2026-05-24 12:10:58 -0300

    feat: exercícios

---
+++ exercícios/01_database_tabela.sql
@@ -0,0 +1,131 @@
+-- #########################################################################
+-- ##                                                                     ##
+-- ##                     EXERCÍCIO DE MYSQL                              ##
+-- ##                                                                     ##
+-- ## Disciplina : Banco de Dados I                                       ##
+-- ## Tema       : Criação de Database e Tabelas                          ##
+-- ##                                                                     ##
+-- #########################################################################
+
+
+
+-- #########################################################################
+-- EXERCÍCIO
+-- #########################################################################
+--
+-- Uma escola precisa de um sistema para cadastrar alunos.
+--
+-- Você deverá desenvolver os comandos SQL necessários
+-- para criar o banco de dados e a tabela.
+--
+-- #########################################################################
+--
+-- REQUISITOS:
+--
+-- 1. Criar um database chamado:
+--    db_escola
+--
+-- 2. Selecionar o database criado.
+--
+-- 3. Criar uma tabela chamada:
+--    aluno
+--
+-- 4. A tabela deverá possuir os seguintes campos:
+--
+--    id_aluno
+--    -> inteiro
+--    -> chave primária
+--    -> auto incremento
+--
+--    nome
+--    -> texto com até 100 caracteres
+--    -> obrigatório
+--
+--    email
+--    -> texto com até 150 caracteres
+--
+--    idade
+--    -> número inteiro
+--
+--    cidade
+--    -> texto com até 100 caracteres
+--
+--    telefone
+--    -> texto com até 20 caracteres
+--
+--    data_cadastro
+--    -> data e hora automática
+--
+-- #########################################################################
+--
+-- DESAFIO EXTRA:
+--
+-- Após criar a tabela:
+--
+-- 1. Mostrar todas as tabelas do database.
+--
+-- 2. Exibir a estrutura da tabela aluno.
+--
+-- #########################################################################
+
+
+
+
+
+-- #########################################################################
+-- ESPAÇO PARA RESPOSTA DO ALUNO
+-- #########################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ exercícios/02_manipular_dados.sql
@@ -0,0 +1,254 @@
+-- #########################################################################
+-- ##                                                                     ##
+-- ##              EXERCÍCIO DE MYSQL - INSERT, UPDATE E DELETE           ##
+-- ##                                                                     ##
+-- ## Disciplina : Banco de Dados I                                       ##
+-- ## Tema       : Criação de tabela e manipulação de dados               ##
+-- ##                                                                     ##
+-- #########################################################################
+
+
+-- #########################################################################
+-- ENUNCIADO
+-- #########################################################################
+--
+-- Uma escola precisa cadastrar alunos e realizar operações básicas
+-- de inserção, alteração, consulta e exclusão de dados.
+--
+-- Resolva as atividades abaixo utilizando comandos SQL.
+--
+-- #########################################################################
+
+
+-- ATIVIDADE 01
+-- Crie um database chamado db_escola_atividade.
+
+
+-- ATIVIDADE 02
+-- Selecione o database db_escola_atividade.
+
+
+-- ATIVIDADE 03
+-- Crie uma tabela chamada aluno com os campos:
+--
+-- id_aluno INT AUTO_INCREMENT PRIMARY KEY
+-- nome VARCHAR(100) NOT NULL
+-- email VARCHAR(150)
+-- idade INT
+-- cidade VARCHAR(100)
+-- curso VARCHAR(100)
+-- data_cadastro DATETIME DEFAULT CURRENT_TIMESTAMP
+
+
+-- ATIVIDADE 04
+-- Exiba todas as tabelas do database.
+
+
+-- ATIVIDADE 05
+-- Exiba a estrutura da tabela aluno.
+
+
+-- ATIVIDADE 06
+-- Insira um aluno chamado Fulano de Tal.
+
+
+-- ATIVIDADE 07
+-- Insira um aluno chamado Ciclano da Silva.
+
+
+-- ATIVIDADE 08
+-- Insira um aluno chamado Beltrano dos Santos.
+
+
+-- ATIVIDADE 09
+-- Insira mais 3 alunos em um único comando INSERT.
+
+
+-- ATIVIDADE 10
+-- Consulte todos os alunos cadastrados.
+
+
+-- ATIVIDADE 11
+-- Consulte apenas os campos nome, idade e cidade.
+
+
+-- ATIVIDADE 12
+-- Consulte apenas os alunos da cidade de Joinville.
+
+
+-- ATIVIDADE 13
+-- Consulte os alunos com idade maior ou igual a 18 anos.
+
+
+-- ATIVIDADE 14
+-- Consulte os alunos ordenados pelo nome em ordem crescente.
+
+
+-- ATIVIDADE 15
+-- Altere a cidade do aluno Fulano de Tal para Blumenau.
+
+
+-- ATIVIDADE 16
+-- Altere o curso do aluno Ciclano da Silva para Banco de Dados I.
+
+
+-- ATIVIDADE 17
+-- Altere a idade do aluno Beltrano dos Santos para 25 anos.
+
+
+-- ATIVIDADE 18
+-- Altere o email de um aluno utilizando o id_aluno como filtro.
+
+
+-- ATIVIDADE 19
+-- Exclua um aluno utilizando o id_aluno como filtro.
+
+
+-- ATIVIDADE 20
+-- Consulte novamente todos os alunos para verificar as alterações.
+
+
+-- ATIVIDADE 21
+-- Conte quantos alunos existem cadastrados na tabela.
+
+
+-- ATIVIDADE 22
+-- Consulte alunos cujo nome contenha a palavra Silva.
+
+
+-- ATIVIDADE 23
+-- Consulte alunos com idade entre 18 e 30 anos.
+
+
+-- ATIVIDADE 24
+-- Exclua todos os alunos da cidade de Curitiba.
+
+
+-- ATIVIDADE 25
+-- Exiba novamente todos os alunos cadastrados.
+
+
+-- #########################################################################
+-- ESPAÇO PARA RESPOSTA DO ALUNO
+-- #########################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
